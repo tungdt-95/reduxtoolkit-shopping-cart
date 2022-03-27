7f9ab6a7c23b42a11204b6e47561097ffdc27b26
tungdt-95 <doantrantung@gmail.com>
2022-03-27 15:08:34 +0700

    update code

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux';
 import store from "./store/store";
 import './scss/index.scss';
@@ -9,9 +9,9 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </Provider>
 
   </React.StrictMode>,
